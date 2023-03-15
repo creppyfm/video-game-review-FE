282d7980d7a627c8b26aa3c68b9b823adada164f
creppyfm <97473588+creppyfm@users.noreply.github.com>
2023-03-15 16:26:28 -0400

    modified URI

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://localhost:8080',
+    baseURL:'http://ec2-107-20-176-108.compute-1.amazonaws.com:8080/api/v1/games',
     headers: {"ngrok-skip-browser-warning": "true"}
 })
