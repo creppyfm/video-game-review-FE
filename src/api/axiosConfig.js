92f264b8d109e877e0320b82e366946f13e58231
creppyfm <97473588+creppyfm@users.noreply.github.com>
2023-04-13 20:09:05 -0400

    updated baseURL in axiosConfig

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://api.notsteam.games', 
+    // baseURL:'http://localhost:8080/api/v1/games',
+    baseURL:'https://notsteam.games', 
     headers: {"ngrok-skip-browser-warning": "true"}
 })
